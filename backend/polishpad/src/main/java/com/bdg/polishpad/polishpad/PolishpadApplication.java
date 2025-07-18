39f5d77d7da5834f511cde38541d7757b889ce9c
Luan-Pinheiro <lpazevedodev@gmail.com>
2025-07-18 20:19:56 -0300

    chore: estrutura inicial do backend

---
+++ backend/polishpad/src/main/java/com/bdg/polishpad/polishpad/PolishpadApplication.java
@@ -0,0 +1,13 @@
+package com.bdg.polishpad.polishpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolishpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolishpadApplication.class, args);
+	}
+
+}
---
+++ backend/polishpad/src/test/java/com/bdg/polishpad/polishpad/PolishpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdg.polishpad.polishpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolishpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
